02fe86e107e7b33c1f33557c91dd8a0d2b59adc1
harihsur <harihsur@amazon.com>
2021-01-01 20:46:48 +0530

    auth commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import React from "react";
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut, AmplifyGreetings} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+Amplify.configure(awsconfig);
+
+const App = () => {
+
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+          setAuthState(nextAuthState);
+          setUser(authData)
+      });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App" style={{background: 'red', color: 'blue'}}>
+        <AmplifyGreetings username={user.username}></AmplifyGreetings>
+      </div>
+    ) : (
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignUp headerText = "MES Group Workforce Manager - Sign Up"
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: "email",
+            label: "User",
+            placeholder: "email",
+            required: true
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "password",
+            required: true
+          },
+          {
+            type: "text",
+            label: "Type",
+            placeholder: "admin / worker",
+            required: true
+          },
+        ]} 
+      />
+      <AmplifySignIn  headerText="MES Group Workforce Manager - Login" slot="sign-in" usernameAlias="email"/>
+    </AmplifyAuthenticator>
   );
-}
+};
 
-export default withAuthenticator(App);
-
-//export default App;
+export default App;
